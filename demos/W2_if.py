a12b790d6b31629b12ed9c4e6321849fa18bddb9
Cristian Constantin <88332555+CristianCtin@users.noreply.github.com>
2021-10-27 10:23:38 +0000

    Online Module

---
+++ demos/W2_if.py
@@ -0,0 +1,11 @@
+name = input('Enter your name: ')
+
+if len(name) > 8:
+  print('you have a loong name! Can i use a nickname please?')#Indented block
+elif name == 'Max'
+  print('Thats a boring name!')
+elif name #TO BE CONTINUED !!!!!!!!!!!!!!!!!!!!
+else:
+  print('Oh, your name is pretty short')
+print('Nevertheless, welcome to the sesion {}!'.format(name))
+
--- demos/basics/input/data_types.py
+++ demos/basics/input/data_types.py
@@ -1,11 +1,13 @@
 print('What is your name human?')
-name = input()
+name = input() #String reprezentation
 print('How old are you (in years)?')
-age = int(input())
+age = int(input()) #Integer
 print('How tall are you (in meters)?')
 height = float(input())
 print('How much do you weight (in kilograms)?')
 weight = int(input())
 
-BMI = float(weight/height**2)
-print(name, "your age is " + str(age), 'years old and your BMI is' ,BMI)
+BMI = float(weight / height**2)
+print(name, "your age is " + str(age), 'years old and your BMI is', BMI)
+
+#print(f'{name} you are {age} years old. Your BMI is {BMI}')
