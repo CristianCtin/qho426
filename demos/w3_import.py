93e74044ebb1eac62a24c1d0e3bf7006df9bbed8
Cristian Constantin <88332555+CristianCtin@users.noreply.github.com>
2021-11-03 14:01:24 +0000

    School finish week 3

---
+++ demos/w3_import.py
@@ -0,0 +1,4 @@
+from demos.w3_lecture import calc_area as bob
+
+
+print(f"The area of a triangle of base 2cm and heigh 5 cm is {bob(5, 2)} cm^2")
---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+
+print(f"This is a random number: {random.randint(1,100)}")
